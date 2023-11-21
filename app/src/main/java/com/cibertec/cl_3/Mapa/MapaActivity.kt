b6ce7405f85dd802a7c8ebd55b56906134d237fd
JC_Dev <juancarlostorresmiranda96@gmail.com>
2023-11-21 00:08:50 -0500

    :v.1.2

---
+++ app/src/main/java/com/cibertec/cl_3/Mapa/MapaActivity.kt
@@ -0,0 +1,12 @@
+package com.cibertec.cl_3.Mapa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cibertec.cl_3.R
+
+class MapaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapa)
+    }
+}
